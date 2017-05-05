21eeec925220850b46522d723d7fc6f69c193d5a
mklechan <mkchan@gmail.com>
2017-05-05 14:55:31 -0700

    typescript fixes so that imports are not required

--- h2o-core/src/main/resources/www/mli/test_plot/proto.js
+++ h2o-core/src/main/resources/www/mli/test_plot/proto.js
@@ -1,63 +1,71 @@
-function plot_klime(frame_key) {
-    $.ajax({
-        type: "POST",
-        url: "/3/Vis/Stats",
-        data: JSON.stringify({ "graphic": { "type": "stats",
-                "parameters": { "digits": 3, "data": true } },
-            "data": { "uri": frame_key } }),
-        contentType: "application/json",
-        success: function (data) {
-            console.log(data);
-            // plot by klime cluster
-            // click / select id value, generate cluster, permanent pinned row for that query
-            var myChart = echarts.init(document.getElementById('main'));
-            var option = {
-                title: {
-                    text: 'KLime Test'
+var ModelInterpretability;
+(function (ModelInterpretability) {
+    function plot_klime(frame_key) {
+        $.ajax({
+            type: "POST",
+            url: "/3/Vis/Stats",
+            data: JSON.stringify({
+                "graphic": {
+                    "type": "stats",
+                    "parameters": { "digits": 3, "data": true }
                 },
-                tooltip: {
-                    trigger: 'axis'
-                },
-                legend: {
-                    data: ['model predictions', 'klime predictions']
-                },
-                xAxis: { data: data.columns[data.column_names.indexOf("idx")] },
-                yAxis: { min: -0.1 },
-                series: [{
-                        name: 'model_pred',
-                        type: 'line',
-                        data: data.columns[data.column_names.indexOf("model_pred")]
+                "data": { "uri": frame_key }
+            }),
+            contentType: "application/json",
+            success: function (data) {
+                console.log(data);
+                // plot by klime cluster
+                // click / select id value, generate cluster, permanent pinned row for that query
+                var myChart = ECharts.init(document.getElementById('main'));
+                var option = {
+                    title: {
+                        text: 'KLime Test'
                     },
-                    {
-                        name: 'klime_pred',
-                        type: 'line',
-                        data: data.columns[data.column_names.indexOf("predict_klime")]
-                    }
-                ].concat(data.column_names.map(function (x) {
-                    if (x.match('^rc_')) {
-                        return {
-                            name: x,
+                    tooltip: {
+                        trigger: 'axis'
+                    },
+                    legend: {
+                        data: ['model predictions', 'klime predictions']
+                    },
+                    xAxis: { data: data.columns[data.column_names.indexOf("idx")] },
+                    yAxis: { min: -0.1 },
+                    series: [{
+                            name: 'model_pred',
                             type: 'line',
-                            data: data.columns[data.column_names.indexOf(x)],
-                            showSymbol: false,
-                            symbolSize: 0,
-                            hoverAnimation: false,
-                            lineStyle: { normal: { width: 0 } }
-                        };
-                    }
-                })).filter(function (x) { return x; })
-            };
-            myChart.setOption(option);
-        }
-    });
-}
-function main() {
-    var params = new URLSearchParams(document.location.search);
-    var interpret_key = params.get("interpret_key");
-    $.get("/3/InterpretModel/" + interpret_key, function (data) {
-        var frame_id = data.interpret_id.name;
-        console.log(frame_id);
-        plot_klime(frame_id);
-    });
-}
-Zepto(main);
+                            data: data.columns[data.column_names.indexOf("model_pred")]
+                        },
+                        {
+                            name: 'klime_pred',
+                            type: 'line',
+                            data: data.columns[data.column_names.indexOf("predict_klime")]
+                        }
+                    ].concat(data.column_names.map(function (x) {
+                        if (x.match('^rc_')) {
+                            return {
+                                name: x,
+                                type: 'line',
+                                data: data.columns[data.column_names.indexOf(x)],
+                                showSymbol: false,
+                                symbolSize: 0,
+                                hoverAnimation: false,
+                                lineStyle: { normal: { width: 0 } }
+                            };
+                        }
+                    })).filter(function (x) { return x; })
+                };
+                myChart.setOption(option);
+            }
+        });
+    }
+    function main() {
+        var params = new URLSearchParams(document.location.search);
+        var interpret_key = params.get("interpret_key");
+        $.get("/3/InterpretModel/" + interpret_key, function (data) {
+            var frame_id = data.interpret_id.name;
+            console.log(frame_id);
+            plot_klime(frame_id);
+        });
+    }
+    ModelInterpretability.main = main;
+})(ModelInterpretability || (ModelInterpretability = {}));
+Zepto(ModelInterpretability.main);
--- h2o-core/src/main/resources/www/mli/test_plot/proto.ts
+++ h2o-core/src/main/resources/www/mli/test_plot/proto.ts
@@ -1,69 +1,74 @@
-import * as echarts from 'echarts';
-function plot_klime(frame_key:string) {
-    $.ajax({
-        type:"POST",
-        url:"/3/Vis/Stats",
-        data: JSON.stringify({ "graphic":
-             {"type":"stats",
-              "parameters":{"digits":3,"data":true}},
-              "data":{"uri":frame_key}}),
-        contentType:"application/json",
-        success: function(data) {
-            console.log(data)
-            // plot by klime cluster
-            // click / select id value, generate cluster, permanent pinned row for that query
-            var myChart = echarts.init(<HTMLDivElement>document.getElementById('main'))
-            var option = {
-                title: {
-                    text: 'KLime Test'
+namespace ModelInterpretability {
+    function plot_klime(frame_key: string) {
+        $.ajax({
+            type: "POST",
+            url: "/3/Vis/Stats",
+            data: JSON.stringify({
+                "graphic":
+                {
+                    "type": "stats",
+                    "parameters": { "digits": 3, "data": true }
                 },
-                tooltip: {
-                    trigger: 'axis'
-                },
-                legend: {
-                    data: ['model predictions', 'klime predictions']
-                },
-                xAxis: {data: data.columns[data.column_names.indexOf("idx")]},
-                yAxis: {min:-0.1},
-                series: [{
+                "data": { "uri": frame_key }
+            }),
+            contentType: "application/json",
+            success:  (data) => {
+                console.log(data)
+                // plot by klime cluster
+                // click / select id value, generate cluster, permanent pinned row for that query
+                const myChart = ECharts.init(<HTMLDivElement>document.getElementById('main'))
+                const option = {
+                    title: {
+                        text: 'KLime Test'
+                    },
+                    tooltip: {
+                        trigger: 'axis'
+                    },
+                    legend: {
+                        data: ['model predictions', 'klime predictions']
+                    },
+                    xAxis: { data: data.columns[data.column_names.indexOf("idx")] },
+                    yAxis: { min: -0.1 },
+                    series: [{
                         name: 'model_pred',
                         type: 'line',
-                        data: data.columns[data.column_names.indexOf("model_pred")] 
+                        data: data.columns[data.column_names.indexOf("model_pred")]
                     },
                     {
                         name: 'klime_pred',
                         type: 'line',
                         data: data.columns[data.column_names.indexOf("predict_klime")]
                     }
-                    ].concat(data.column_names.map(function(x:string) {
-                        if(x.match('^rc_')) {
-                           return  {
-                            name: x,
-                            type: 'line',
-                            data: data.columns[data.column_names.indexOf(x)],
-                            showSymbol: false,
-                            symbolSize: 0,
-                            hoverAnimation:false,
-                            lineStyle: {normal:{width:0}}
-                            } 
-                        } 
-                    })).filter(x=>x)
-            };
-             myChart.setOption(option);
-             
-         }
-    })
-}
-function main():void {
-    var params = new URLSearchParams(document.location.search) 
-    var interpret_key = params.get("interpret_key")
-    $.get("/3/InterpretModel/"+interpret_key,
-       function(data) {
-           var frame_id = data.interpret_id.name
-           console.log(frame_id)
-           plot_klime(frame_id)
-       }
-    )
+                    ].concat(data.column_names.map(function (x: string) {
+                        if (x.match('^rc_')) {
+                            return {
+                                name: x,
+                                type: 'line',
+                                data: data.columns[data.column_names.indexOf(x)],
+                                showSymbol: false,
+                                symbolSize: 0,
+                                hoverAnimation: false,
+                                lineStyle: { normal: { width: 0 } }
+                            }
+                        }
+                    })).filter(x => x)
+                };
+                myChart.setOption(option);
+
+            }
+        })
+    }
+    export function main(): void {
+        const params = new URLSearchParams(document.location.search)
+        const interpret_key = params.get("interpret_key")
+        $.get("/3/InterpretModel/" + interpret_key,
+            function (data) {
+                var frame_id = data.interpret_id.name
+                console.log(frame_id)
+                plot_klime(frame_id)
+            }
+        )
+    }
 }
 
-Zepto(main)
+Zepto(ModelInterpretability.main)
